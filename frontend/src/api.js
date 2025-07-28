8cfb8d97cfd7f4c7ecceae0af2ae49e6f307ed75
binkhamis22 <crazy.cool2030@gmail.com>
2025-07-28 23:07:01 +0300

    Add debug logging to frontend API calls to troubleshoot admin dashboard

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -19,21 +19,48 @@ const API = {
   // Products
   getProducts: () => fetch(`${API_BASE_URL}/api/products`).then(res => res.json()),
   
-  createProduct: (productData) => fetch(`${API_BASE_URL}/api/products`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(productData)
-  }).then(res => res.json()),
+  createProduct: (productData) => {
+    console.log('Creating product:', productData);
+    return fetch(`${API_BASE_URL}/api/products`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(productData)
+    }).then(res => {
+      console.log('Create product response status:', res.status);
+      return res.json();
+    }).catch(err => {
+      console.error('Create product error:', err);
+      throw err;
+    });
+  },
 
-  updateProduct: (id, productData) => fetch(`${API_BASE_URL}/api/products/${id}`, {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(productData)
-  }).then(res => res.json()),
+  updateProduct: (id, productData) => {
+    console.log('Updating product:', id, productData);
+    return fetch(`${API_BASE_URL}/api/products/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(productData)
+    }).then(res => {
+      console.log('Update product response status:', res.status);
+      return res.json();
+    }).catch(err => {
+      console.error('Update product error:', err);
+      throw err;
+    });
+  },
 
-  deleteProduct: (id) => fetch(`${API_BASE_URL}/api/products/${id}`, {
-    method: 'DELETE'
-  }).then(res => res.json()),
+  deleteProduct: (id) => {
+    console.log('Deleting product:', id);
+    return fetch(`${API_BASE_URL}/api/products/${id}`, {
+      method: 'DELETE'
+    }).then(res => {
+      console.log('Delete product response status:', res.status);
+      return res.json();
+    }).catch(err => {
+      console.error('Delete product error:', err);
+      throw err;
+    });
+  },
 
   // Orders
   getOrders: () => fetch(`${API_BASE_URL}/api/orders`).then(res => res.json()),
@@ -46,15 +73,33 @@ const API = {
     body: JSON.stringify(orderData)
   }).then(res => res.json()),
 
-  updateOrder: (id, updateData) => fetch(`${API_BASE_URL}/api/orders/${id}`, {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(updateData)
-  }).then(res => res.json()),
+  updateOrder: (id, updateData) => {
+    console.log('Updating order:', id, updateData);
+    return fetch(`${API_BASE_URL}/api/orders/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updateData)
+    }).then(res => {
+      console.log('Update order response status:', res.status);
+      return res.json();
+    }).catch(err => {
+      console.error('Update order error:', err);
+      throw err;
+    });
+  },
 
-  deleteOrder: (id) => fetch(`${API_BASE_URL}/api/orders/${id}`, {
-    method: 'DELETE'
-  }).then(res => res.json()),
+  deleteOrder: (id) => {
+    console.log('Deleting order:', id);
+    return fetch(`${API_BASE_URL}/api/orders/${id}`, {
+      method: 'DELETE'
+    }).then(res => {
+      console.log('Delete order response status:', res.status);
+      return res.json();
+    }).catch(err => {
+      console.error('Delete order error:', err);
+      throw err;
+    });
+  },
 
   // Debug
   debugUsers: () => fetch(`${API_BASE_URL}/api/auth/debug`).then(res => res.json()),
